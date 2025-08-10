af530d16e12c482e6402c39b0085380bf0a95291
unknown <snandakumar@gmail.com>
2025-08-10 10:30:32 +0530

    minor typo in argument to create_engine, corrected.

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
 
-engine          =       create_engine(DATABASE_URL,pre_pool_ping=True)
+engine          =       create_engine(DATABASE_URL,pool_pre_ping=True)
 SessionLocal    =       sessionmaker(bind       = engine,
                                      autoflush  = False,
                                      autocommit = False)
